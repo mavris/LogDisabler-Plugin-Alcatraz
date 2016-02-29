0c7fb217fac18b36d7240d31e8565470e51a11e3
Michael Mavris <michaelmavris@gmail.com>
2016-02-29 09:12:33 +0200

    Update LogController.m
    Fix a minor bug for EnableLogs

--- LogDisabler/LogController.m
+++ LogDisabler/LogController.m
@@ -85,7 +85,7 @@
     
     NSTask *task = [[NSTask alloc] init];
     task.launchPath = @"/usr/bin/find";
-    task.arguments = @[ projectPath,@"-not" ,@"-path" ,@"*/\\.*", @"-type", @"f", @"-name", @"*.m", @"-exec", @"sed", @"-i", @"", @"s!NSLog(!//LDLog(!", @"{}", @"+" ];
+    task.arguments = @[ projectPath,@"-not" ,@"-path" ,@"*/\\.*", @"-type", @"f", @"-name", @"*.m", @"-exec", @"sed", @"-i", @"", @"s!//LDLog(!NSLog(!", @"{}", @"+" ];
     [task launch];
     [task waitUntilExit];
     
